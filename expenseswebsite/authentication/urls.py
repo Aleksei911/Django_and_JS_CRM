b284f68a9f01ed66acc2715c93d5f2c0200cb69f
Aleksei Kohnuk <kohnuk92@gmail.com>
2023-06-10 16:06:04 +0300

    added email validation

--- expenseswebsite/authentication/urls.py
+++ expenseswebsite/authentication/urls.py
@@ -1,9 +1,10 @@
-from .views import RegistrationView, UsernameValidationView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView
 from django.urls import path
 
 
 urlpatterns = [
     path('register', RegistrationView.as_view(), name='register'),
     path('validate-username', UsernameValidationView.as_view(), name='validate-username'),
+    path('validate-email', EmailValidationView.as_view(), name='validate-email'),
 ]
 
--- expenseswebsite/expenseswebsite/static/js/register.js
+++ expenseswebsite/expenseswebsite/static/js/register.js
@@ -1,8 +1,37 @@
 const usernameField = document.querySelector('#usernameField');
 const feedBackArea = document.querySelector('.invalid_feedback');
+const emailField = document.querySelector('#emailField');
+const emailFeedBackArea = document.querySelector('.emailFeedBackArea');
+const usernameSuccessOutput = document.querySelector('.usernameSuccessOutput');
+
+
+emailField.addEventListener('keyup', (e) => {
+    const emailVal = e.target.value;
+
+    emailField.classList.remove('is-invalid');
+    emailFeedBackArea.style.display = 'none';
+
+    if (emailVal.length > 0) {
+        fetch('/authentication/validate-email', {
+            body: JSON.stringify({email: emailVal}),
+            method: 'POST',
+        }).then(res => res.json()).then(data => {
+            console.log('data', data);
+            if (data.email_error) {
+                emailField.classList.add('is-invalid');
+                emailFeedBackArea.style.display = 'block';
+                emailFeedBackArea.innerHTML = '<p>${data.email_error}</p>';
+            }
+        });
+    }
+});
+
+
+
 usernameField.addEventListener('keyup', (e) => {
-    console.log('7777', 7777);
     const usernameVal = e.target.value;
+    usernameSuccessOutput.style.display = 'block';
+    usernameSuccessOutput.textContent = `Checking ${usernameVal}`;
 
     usernameField.classList.remove('is-invalid');
     feedBackArea.style.display = 'none';
