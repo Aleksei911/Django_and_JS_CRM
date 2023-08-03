b106cd971d36b2e068198d90301c32769458af79
Aleksei Kohnuk <kohnuk92@gmail.com>
2023-08-03 11:32:00 +0300

    add activate a users account

--- expenseswebsite/authentication/urls.py
+++ expenseswebsite/authentication/urls.py
@@ -1,9 +1,10 @@
-from .views import RegistrationView, UsernameValidationView, EmailValidationView, VerificationView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView, VerificationView, LoginView
 from django.urls import path
 
 
 urlpatterns = [
-    path('register', RegistrationView.as_view(), name='register'),
+    path('register', LoginView.as_view(), name='register'),
+    path('login', RegistrationView.as_view(), name='login'),
     path('validate-username', UsernameValidationView.as_view(), name='validate-username'),
     path('validate-email', EmailValidationView.as_view(), name='validate-email'),
     path('activate/<uidb64>/<token>', VerificationView.as_view(), name='activate'),
