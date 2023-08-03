cb63057b930a3b3af1669f040e6157ffe0988a91
Aleksei Kohnuk <kohnuk92@gmail.com>
2023-08-03 12:23:08 +0300

    add login, logout and access control

--- expenseswebsite/authentication/urls.py
+++ expenseswebsite/authentication/urls.py
@@ -1,12 +1,12 @@
-from .views import RegistrationView, UsernameValidationView, EmailValidationView, VerificationView, LoginView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView, VerificationView, LoginView, \
+    LogoutView
 from django.urls import path
 
-
 urlpatterns = [
-    path('register', LoginView.as_view(), name='register'),
-    path('login', RegistrationView.as_view(), name='login'),
+    path('register', RegistrationView.as_view(), name='register'),
+    path('login', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
     path('validate-username', UsernameValidationView.as_view(), name='validate-username'),
     path('validate-email', EmailValidationView.as_view(), name='validate-email'),
     path('activate/<uidb64>/<token>', VerificationView.as_view(), name='activate'),
 ]
-
