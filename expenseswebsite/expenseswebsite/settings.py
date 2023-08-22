3ae3ab537ae17ad4d59e6ca72b63f06e4276cb27
Aleksei Kohnuk <kohnuk92@gmail.com>
2023-08-22 12:39:28 +0300

    start new app; create model, view, urls and template; create currencies.json

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'expenses.apps.ExpensesConfig',
+    'userpreferences.apps.UserpreferencesConfig',
 ]
 
 MIDDLEWARE = [
