5717dba3ad2038c5cdf8f2576d91b2e74cd8cabd
Aleksei Kohnuk <kohnuk92@gmail.com>
2023-09-01 17:00:38 +0300

    update: exporting data to csv

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -1,11 +1,12 @@
 import json
 import datetime
+import csv
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
 from django.contrib import messages
 from django.core.paginator import Paginator
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from userpreferences.models import UserPreferences
 
 
@@ -143,3 +144,18 @@ def expense_category_summary(request):
 
 def stats_view(request):
     return render(request, 'expenses/stats.html')
+
+
+def export_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=Expenses' + str(datetime.datetime.now()) + '.csv'
+
+    writer = csv.writer(response)
+    writer.writerow(['Amount', 'Description', 'Category', 'Date'])
+
+    expenses = Expense.objects.filter(owner=request.user)
+
+    for expense in expenses:
+        writer.writerow([expense.amount, expense.description, expense.category, expense.date])
+
+    return response
