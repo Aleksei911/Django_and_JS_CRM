abd74304027eaf90aacf799899182f8cc3b50c9c
Aleksei Kohnuk <kohnuk92@gmail.com>
2023-09-03 12:45:51 +0300

    update: exporting data to .xls

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -1,6 +1,7 @@
 import json
 import datetime
 import csv
+import xlwt
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Category, Expense
@@ -157,5 +158,31 @@ def export_csv(request):
 
     for expense in expenses:
         writer.writerow([expense.amount, expense.description, expense.category, expense.date])
+
+    return response
+
+
+def export_excel(request):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename=Expenses' + str(datetime.datetime.now()) + '.xls'
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Expenses')
+    row_num = 0
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    columns = ['Amount', 'Description', 'Category', 'Date']
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    font_style = xlwt.XFStyle()
+    rows = Expense.objects.filter(owner=request.user).values_list('Amount', 'Description', 'Category', 'Date')
+
+    for row in rows:
+        row_num+=1
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, str(row[col_num]), font_style)
+    wb.save(response)
 
     return response
